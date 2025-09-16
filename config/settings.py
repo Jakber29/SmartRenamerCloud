1e0b23b0f64bb596bac87f2312627670debc6bce
Jake Berman <jakeberman@Jakes-Mac-mini.local>
2025-09-15 17:42:01 -0700

    Allow any host in ALLOWED_HOSTS for deployment flexibility

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qn5iigya9dx@ndxr++!%)fqbr_xltd#n=+f@0udgv2nfrt22aj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
